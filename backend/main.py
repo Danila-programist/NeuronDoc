8750d3ce8af03482ce464c2441e27401cd17b403
Danila Golubev <danila25golubev@gmail.com>
2025-11-13 21:51:51 +0300

    feat: add new file main.py

---
+++ backend/main.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+
+
+app = FastAPI(
+    title="Анализатор текста",
+    description="API для работы с проектом NeuronDoc",
+    version="1.0.0",
+)
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app="main:app", host="0.0.0.0", port=8000, reload=True)
